db749120a8c73f4c351b69306d4dd95e121edc29
eduardoforca <edusousa1996@hotmail.com>
2017-07-23 02:45:36 -0300

    Banco de Dados

--- medicao/medicao_invasivo.py
+++ medicao/medicao_invasivo.py
@@ -1,6 +1,9 @@
 # Simple example of reading the MCP3008 analog input channels and 
 # printing them all out. Author: Tony DiCola License: Public Domain
 import time
+import datetime
+import string
+import requests
 
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
@@ -25,7 +28,7 @@ sumV = 0
 contador = 0
 offset = 512
 n = 0
-sensibilidade = 0.1
+sensibilidade = 0.066
 acc = 0
 
 print('Reading MCP3008 values, press Ctrl-C to quit...')
@@ -38,15 +41,32 @@ while contador < 10:
 	contador = contador + 1
 offset = acc/10
 
-period = 1/60
-t_start = time.time()
-count = 0
+#period = 1/60
+#t_start = time.time()
+#count = 0
+tempo_intervalo = 500
+contador_amostra = 0
+dados_consumo={
+	'tensao': [],
+	'corrente': [],
+	'data_medicao': [],
+	'id_boltz': 1,
+	'num_seq': -1
+}
+
+contador_mult = 1
+ativo = False
 
 while True:
-    amostras = 500
-    for n in range(1,amostras - 1):  
+    end = False
+    #amostras = 500
+    contador = 0
+    tempo_inicial = time.time()*1000
+    while time.time()*1000 <= (tempo_inicial + tempo_intervalo):
+    #for n in range(1,amostras - 1):  
        # Read all the ADC channel values in a list.
        values = [0]*8
+       contador += 1
        for i in range(8):
            # The read_adc function will get the value of the specified channel (0-7).
            values[i] = mcp.read_adc(i)
@@ -55,11 +75,61 @@ while True:
        tensao = medicao - offset
        sqV = tensao*tensao
        sumV += sqV
-    Vrms = (sumV/amostras)**(0.5)
+    Vrms = (sumV/contador)**(0.5)
     Irms = (5.0/1023)*Vrms/sensibilidade
        # Print the ADC values.
        #print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
        # Pause for half a second.
     sumV = 0
-    print Irms
-    time.sleep(0.0002)  
+    if (ativo == False):
+	if Irms >= 0.2:
+		ativo = True
+		dados_consumo['num_seq'] += 1
+	else:
+		ativo = False
+    else:
+	if Irms < 0.2:
+		print ("Passou")
+		end = True
+		ativo = False
+
+    if ativo:
+    	contador_amostra += 1
+  	dados_consumo['tensao'].append(220)
+	dados_consumo['corrente'].append(Irms)
+	dados_consumo['data_medicao'].append(str(datetime.datetime.now()))
+    	if contador_amostra%10 == 0:
+		print dados_consumo
+		dados_consumo['tensao'] = []
+		dados_consumo['corrente'] = []
+		dados_consumo['data_medicao'] = []
+		contador_mult += 1
+		email = "maximillianfx@gmail.com"
+		senha = "220494max"
+		cabecalho = {'from': email, 'password': senha}
+		URL = 'http://192.168.42.59:8000/v1/autorizar'
+		URL_consumo = 'http://192.168.42.59:8000/v1/consumo/'
+		medicoes_url = '/medicoes/'
+		response_auth = requests.get(URL, headers=cabecalho)
+		token = str(response_auth.headers['app_token'])
+		print token
+		cabecalho2 = {'authorization': "token " + token}
+		URL_final = URL_consumo + "1" + medicoes_url
+		response_post =  requests.post(URL_final,headers=cabecalho2,data=dados_consumo)
+		if response_post.status_code != 201:
+			print ("Fallha")
+    else:
+	print ("Oi")
+	if end:
+		print ("Ola")
+		restantes = (10*contador_mult) - contador_amostra
+		for m in range(restantes):
+			dados_consumo['tensao'].append(220)
+			dados_consumo['corrente'].append(0)
+			dados_consumo['data_medicao'].append(str(datetime.datetime.now()))
+			contador_amostra += 1
+			end = False
+		print(dados_consumo)
+		#print Irms
+    		#print contador
+    		#time.sleep(0.0002)  
